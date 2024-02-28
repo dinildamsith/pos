249f7c948cd8306f671a18bf684f0541afc38b7c
dinildamsith <dinildamsith52@gmail.com>
2024-02-28 17:26:47 +0530

    customer directory add and import files✔️

---
+++ src/components/Customer/customer.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import images from "../../constants/image";
+import SideBar from "../SideBar/SideBar";
+import MobileSideBar from "../SideBar/mobileSideBar";
