14f6f3771c00b7f932288f9bc5b9243d72da0b07
dinildamsith <dinildamsith52@gmail.com>
2024-02-28 18:11:10 +0530

    customer logo2 added✔️

--- src/constants/image.js
+++ src/constants/image.js
@@ -6,6 +6,7 @@ import orderLogo from '../assets/img/orderLog.png'
 import orderManage from '../assets/img/orderMangeLogo.png'
 import headSetLogo from '../assets/img/headSet.png'
 import selectItem from '../assets/img/selectItem.png'
+import customerLogo2 from '../assets/img/customerLogo2.png'
 
 const images = {
     dzLogo,
@@ -15,7 +16,8 @@ const images = {
     orderLogo,
     orderManage,
     headSetLogo,
-    selectItem
+    selectItem,
+    customerLogo2
 }
 
 export default images;
