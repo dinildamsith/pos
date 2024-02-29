1c5d56b13764e7b676b25b34e76d21552621dbd5
dinildamsith <dinildamsith52@gmail.com>
2024-03-01 01:42:42 +0530

    ✔️

--- src/App.js
+++ src/App.js
@@ -8,26 +8,6 @@ import OrderManage from "./components/OrderManage/OrderManage";
 import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
 
 function App() {
-// Import necessary modules
-    const express = require('express');
-    const path = require('path');
-
-// Create an Express app
-    const app = express();
-    const PORT = process.env.PORT || 3000;
-
-// Serve static files from the 'build' directory
-    app.use(express.static(path.join(__dirname, 'build')));
-
-// Handle all routes by serving the 'index.html' file
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-
-// Start the server
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
 
 
   return (
