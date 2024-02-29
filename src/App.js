997cb490819d0b1ec47d6c9acc1336eba2bba21d
dinildamsith <dinildamsith52@gmail.com>
2024-03-01 01:40:25 +0530

    404 error fixed✔️

--- src/App.js
+++ src/App.js
@@ -8,39 +8,11 @@ import OrderManage from "./components/OrderManage/OrderManage";
 import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
 
 function App() {
-    // import logo from './logo.svg';
-    import './App.css';
-    import Home from "./components/Home/Home";
-    import Item from "./components/Item/Item";
-    import Customer from "./components/Customer/customer";
-    import Order from "./components/Order/Order";
-    import OrderManage from "./components/OrderManage/OrderManage";
-    import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
-
-    function App() {
-
-
-        return (
-            <div className="App">
-
-
-
-                <Router>
-                    <Routes>
-                        <Route path="/pos" element={<Home />} />
-                        <Route path="/item" element={<Item />} />
-                        <Route path="/customer" element={<Customer />} />
-                        <Route path="/order" element={<Order />} />
-                        <Route path="/orderManage" element={<OrderManage />} />
-                    </Routes>
-                </Router>
-
-            </div>
-        );
-    }
-
+// Import necessary modules
     const express = require('express');
     const path = require('path');
+
+// Create an Express app
     const app = express();
     const PORT = process.env.PORT || 3000;
 
@@ -52,15 +24,13 @@ function App() {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 
+// Start the server
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 
 
-    export default App;
-
-
-    return (
+  return (
     <div className="App">
 
 
