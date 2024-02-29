5bde4efb3a031a3cdb4b4139d430938770b86540
dinildamsith <dinildamsith52@gmail.com>
2024-02-29 12:06:40 +0530

    order page responsive xxl size done✔️

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -74,7 +74,7 @@ function Order(){
 
                     <div className="row">
 
-                        <div className="col-12">
+                        <div className="col-12 col-xxl-3">
                             <div>
                                 <div className="form-group">
                                     <label htmlFor="formGroupExampleInput2" className="text-black fw-bold">Order Id</label>
@@ -102,12 +102,11 @@ function Order(){
                         </div>
 
 
-
-                        <div className="col-12">
-                            <img src={images.selectItem}/>
+                        <div className="col-12 col-xxl-5 ">
+                            <img src={images.selectItem} className="mt-5 mw-100 d-block mx-auto"/>
                         </div>
 
-                        <div className="col-12">
+                        <div className="col-12 col-xxl-4">
 
                             <div>
 
@@ -144,8 +143,8 @@ function Order(){
 
 
 
-                    <div className="col-12">
-                        <h1 className="fw-bold">Cart</h1>
+                    <div className="col-12 col-xxl-8 mt-xxl-4">
+                        <h1 className="fw-bold text-center">Cart</h1>
 
 
                         <table className="table">
@@ -166,9 +165,9 @@ function Order(){
 
 
 
-                    <div className="col-12">
+                    <div className="col-12 col-xxl-2 mt-xxl-5">
 
-                        <div>
+                        <div className="mt-5">
                             <div className="form-group">
                                 <label htmlFor="formGroupExampleInput2" className="text-black fw-bold">Total</label>
                                 <input type="text" className="form-control fw-bold bg-white" id="formGroupExampleInput2" placeholder="" disabled/>
