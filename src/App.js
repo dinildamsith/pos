4e1aaa8d341a2f16465a73974963e6650667c809
dinildamsith <dinildamsith52@gmail.com>
2024-02-29 13:12:22 +0530

    order manage page path set✔️

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 import Item from "./components/Item/Item";
 import Customer from "./components/Customer/customer";
 import Order from "./components/Order/Order";
+import OrderManage from "./components/OrderManage/OrderManage";
 import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
                 <Route path="/item" element={<Item />} />
                 <Route path="/customer" element={<Customer />} />
                 <Route path="/order" element={<Order />} />
+                <Route path="/orderManage" element={<OrderManage />} />
             </Routes>
         </Router>
 
